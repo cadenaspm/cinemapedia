3dc414483675bd8a34697615ff67a346a95742cc
Manuel Ramirez <manuelcadenaspm@gmail.com>
2025-08-20 15:57:00 -0500

    Creation of a data list using Clean Architecture and Dio for data consumption

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
@@ -10,7 +11,10 @@ Future<void> main() async{
 
   await dotenv.load(fileName: '.env');
 
-  runApp(const MyApp());
+  runApp(
+  ProviderScope(
+      child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {

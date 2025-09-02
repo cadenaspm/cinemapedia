66e088b55e48202b381e919bcf6ba0664334e2fe
Manuel Ramirez <manuelcadenaspm@gmail.com>
2025-09-02 05:58:32 -0500

    finish

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,23 +2,34 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
 
-    GoRoute(
-      path: '/',
+    GoRoute( 
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen()
+      builder: (context, state){
+
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+
+        return HomeScreen( pageIndex: pageIndex, ); 
+      },
+      routes: [
+         GoRoute(
+          path: '/movie/:id',
+          name:  MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            
+            return MovieScreen(movieId: movieId);
+          }
+        ),
+      ]
     ),
 
     GoRoute(
-      path: '/movie/:id',
-      name:  MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no-id';
-        
-        return MovieScreen(movieId: movieId);
-      }
+      path: '/',
+      redirect: (_, __) => '/home/0',
     )
   ]
 
---
+++ lib/presentation/views/movies/home_view.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+import '../../widgets/widgets.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final initialLoading = ref.watch(initialLoadingProvider);
+
+    if( initialLoading ) return const FullScreenLoader();
+
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
+    final popularMovies = ref.watch(popularMoviesProvider); 
+    final upcomingMovies = ref.watch(upcomingMoviesProvider); 
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
+
+    if (slideShowMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return CustomScrollView(
+      slivers: [
+
+        SliverPersistentHeader(
+          delegate: CustomAppbar(minExtents: 80, maxExtents: 100),
+          pinned: true,
+          floating: true,
+        ), 
+
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+              children: [
+                
+                MoviesSlideshow(movies: slideShowMovies),
+
+                MovieHorizontalListview(
+                  movies: nowPlayingMovies,
+                  title: 'En cines',
+                  subtitle: 'Lunes 20',
+                  onNextPage: () {
+                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+                  },
+                ),
+
+                MovieHorizontalListview(
+                  movies: popularMovies,
+                  title: 'Populares',
+                  subtitle: 'Este mes',
+                  onNextPage: () {
+                    ref.read(popularMoviesProvider.notifier).loadNextPage();
+                  },
+                ),
+
+                MovieHorizontalListview(
+                  movies: upcomingMovies,
+                  title: 'Proximamente',
+                  onNextPage: () {
+                    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+                  },
+                ),
+
+                MovieHorizontalListview(
+                  movies: topRatedMovies,
+                  title: 'Mejor valoradas',
+                  subtitle: 'Desde 1980',
+                  onNextPage: () {
+                    ref.read(topRatedMoviesProvider .notifier).loadNextPage();
+                  },
+                ),
+                SizedBox(height: 20),
+              ],
+            );
+          }, childCount: 1),
+        ),
+      ],
+    );
+  }
+}
